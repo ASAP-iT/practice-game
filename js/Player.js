ef8b480468c359e0585955f7647e35ee01bec6f6
Fruttis1337 <Fruttis1337@mail.ru>
2021-07-31 01:37:06 +1000

    Update Player.js

--- js/Player.js
+++ js/Player.js
@@ -1,37 +1,37 @@
 class Player {
 
     constructor(x, y, dx, dy) {
-        this.x = x;
-        this.y = y;
-        this.dx = dx;
-        this.dy = dy;
+        this.x = x
+        this.y = y
+        this.dx = dx
+        this.dy = dy
     }
 
     draw(z) {
         /*var pimg = new Image();
         pimg.src = 'img/witch.gif';
         context.drawImage(pimg, this.x, this.y, 75, 75);*/
-        context.drawImage(Img[z], this.x, this.y, 75, 75);
-        context.fillStyle = '#f20000';
-        context.fillRect(this.x + 75 / 2 + 3, this.y + 75 / 2, 10, 10);
+        context.drawImage(Img[z], this.x, this.y, 75, 75)
+        context.fillStyle = '#f20000'
+        context.fillRect(this.x + 75 / 2 + 3, this.y + 75 / 2, 10, 10)
     }
 
-    move(borders, key_released, context) {
-        if(key_released === "left" && this.x - this.dx >= borders[0])
-        {
+    move(borders, platform, key_released, context) {
+        if(key_released === "left" && this.x - this.dx >= borders[0]) {
             //todo сделать отрисовку
             this.x -= this.dx;
         }
-        if(key_released === "right" && this.x + this.dx <= borders[3])
-        {
+        if(key_released === "right" && this.x + this.dx <= borders[3]) {
             //todo сделать отрисовку
             this.x += this.dx;
         }
-        if(key_released === "up" && this.y - this.dy >= borders[1])
-        {
+        if(key_released === "up" && this.y - this.dy >= borders[1] && (this.x )) {
             //todo сделать отрисовку
             this.y -= this.dy;
         }
+
+    }
+    atach(){
 
     }
 }

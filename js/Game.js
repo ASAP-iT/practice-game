20f7716dbb38f88c8cfd7387d442e039a44f237d
RomaOkorosso <zatrutin.roma.work@gmail.com>
2021-07-31 06:10:23 +1000

    zalupa iz-pod HORSE

--- js/Game.js
+++ js/Game.js
@@ -1,10 +1,11 @@
 function include(url) {
     const script = document.createElement('script');
     script.src = url;
-    document.getElementsByTagName('head')[0].appendChild(script);
+    document.head.appendChild(script);
 }
+import "./Wizard.js";
+include("js/Wizard.js");
 
-include("Wizard.js");
 
 
 const canvas = document.getElementById('game');
@@ -12,11 +13,17 @@ const context = canvas.getContext('2d');
 document.addEventListener('keydown', keyPressed);
 document.addEventListener('keyup', keyUnpressed);
 
+var pl = {x: 100, y: 300, dx: 10, dy: 10};
+var player = new Wizard(pl.x, pl.y, pl.dx, pl.dy);
+
 const LEFT_KEY = 65;
 const RIGHT_KEY = 68;
 const SPACE_KEY = 32;
-let rightKeyPressed, leftKeyPressed, spaceKeyPressed;
+const ENTER_KEY = 13;
+let rightKeyPressed, leftKeyPressed, spaceKeyPressed, enterKeyPressed;
 let counter = 0;
+const background = new Image();
+background.src = 'backgrounds/backgroundarenablue2.png';
 
 function keyPressed(event) {
 
@@ -26,12 +33,14 @@ function keyPressed(event) {
     if (event.keyCode === RIGHT_KEY) {
         rightKeyPressed = true;
     }
+    if (event.keyCode === ENTER_KEY) {
+        enterKeyPressed = true;
+    }
     if (event.keyCode === SPACE_KEY) {
         spaceKeyPressed = true;
         if (counter < 100) {
             counter += 10;
-        }
-        else {
+        } else {
             spaceKeyPressed = false;
         }
     }
@@ -45,7 +54,44 @@ function keyUnpressed(event) {
     if (event.keyCode === RIGHT_KEY) {
         rightKeyPressed = false;
     }
+    if (event.keyCode === ENTER_KEY) {
+        enterKeyPressed = false;
+    }
     if (event.keyCode === SPACE_KEY && spaceKeyPressed === true) {
         spaceKeyPressed = false;
     }
 }
+
+console.log("HUI");
+
+background.onload = function () {
+    game();
+}
+
+function game() {
+    update();
+    render();
+    requestAnimFrame(game);
+}
+
+
+function update() {
+
+}
+
+function render() {
+
+    context.drawImage(background, 0, 0, 1200, 700);
+    // player.draw(0);
+}
+
+var requestAnimFrame = (function () {
+    return window.requestAnimationFrame ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimatoinFrame ||
+        window.oRequestAnimationFrame ||
+        window.msRequestAnimatoinFrame ||
+        function (callback) {
+            window.setTimeout(callback, 1000 / 20);
+        };
+})();
--- js/Wizard.js
+++ js/Wizard.js
@@ -9,10 +9,12 @@ class Wizard {
     }
 
     draw(z) {
+        let img = new Image();
+        img.src = "playerAnim/WizardImg/mage.png";
         /*var pimg = new Image();
         pimg.src = 'img/witch.gif';
         context.drawImage(pimg, this.x, this.y, 75, 75);*/
-        context.drawImage(Img[0], this.x, this.y, 75, 75)
+        context.drawImage(img, this.x, this.y, 75, 75)
         context.fillStyle = '#f20000'
         context.fillRect(this.x + 75 / 2 + 3, this.y + 75 / 2, 10, 10)
     }
